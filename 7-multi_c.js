84b7f774a74ab69413b2ada9917407d5b95dd14c
Lois Aideloje <loisaideloje@gmail.com>
2025-05-24 00:46:32 +0100

    Print phrase multiple times from argument

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2);
 const times = parseInt(args[0]);
 
-if (isNaN(times) || times <= 0) {
+if (isNaN(times)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (times > 0) {
   for (let i = 0; i < times; i++) {
     console.log("C is fun");
   }
-
+}
